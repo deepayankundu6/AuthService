2a3e580332ce0a65ce5ad5605e861da42776464d
Deepayan Kundu <deepayankundu6@gmail.com>
2024-01-20 11:21:56 +0530

    fix

--- getCred.sh
+++ getCred.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$1/prod/users/authenticate"  -H "x-api-key: $2"
 RESPONSE=$(curl -X GET "$1/prod/users/authenticate"  -H "x-api-key: $2")
-export AWS_DEFAULT_REGION = ap-south-1
-export AWS_SESSION_TOKEN  = $(jq -r '.aws_session_token' <<<"$RESPONSE")
-export AWS_ACCESS_KEY_ID = $(jq -r '.aws_access_key_id' <<<"$RESPONSE") 
-export AWS_SECRET_ACCESS_KEY = $(jq -r '.aws_secret_access_key' <<<"$RESPONSE")
-export AWS_DEFAULT_OUTPUT = json
+export AWS_DEFAULT_REGION=ap-south-1
+export AWS_SESSION_TOKEN= $(jq -r '.aws_session_token' <<<"$RESPONSE")
+export AWS_ACCESS_KEY_ID= $(jq -r '.aws_access_key_id' <<<"$RESPONSE") 
+export AWS_SECRET_ACCESS_KEY= $(jq -r '.aws_secret_access_key' <<<"$RESPONSE")
+export AWS_DEFAULT_OUTPUT=json
 
 
